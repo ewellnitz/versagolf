e49c3ccfdcd80ca28947b0bbffd805687b4ac898
Eric Wellnitz <me@ericwellnitz.com>
2017-10-01 14:32:20 -0400

    tee type work enum vs int.

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -9,10 +9,20 @@ import { Course } from '../../models/course';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
+  teeTypes: Array<TeeType> = [];
   course: Course = <Course>{};
   private id: any;
   constructor(private route: ActivatedRoute,
-    private router: Router, private courseService: CourseService) { }
+    private router: Router, private courseService: CourseService) {
+    // create teetypes
+    this.teeTypes.push({ id: 1, name: 'Black', selected: false });
+    this.teeTypes.push({ id: 2, name: 'Blue', selected: false });
+    this.teeTypes.push({ id: 3, name: 'Gold', selected: false });
+    this.teeTypes.push({ id: 4, name: 'Green', selected: false });
+    this.teeTypes.push({ id: 5, name: 'Red', selected: false });
+    this.teeTypes.push({ id: 6, name: 'White', selected: false });
+    this.teeTypes.push({ id: 7, name: 'Yellow', selected: false });
+  }
 
   ngOnInit() {
     this.route
@@ -34,7 +44,7 @@ export class CourseComponent implements OnInit {
       return;
     }
     this.courseService.getCourse(this.id)
-      .subscribe( course => {
+      .subscribe(course => {
         this.setEditCourse(course);
       },
       error => {
@@ -45,12 +55,55 @@ export class CourseComponent implements OnInit {
   private setEditCourse(course: Course) {
     if (course) {
       this.course = course;
-      console.log(course);
+      // since all holes should have the same tee types, only inspect the first hole
+      const hole = this.course.holes[0];
+
+      // select tee types based on existing holes
+      this.teeTypes.forEach((teeType) => {
+        // find the hole tee for this hole
+        const value = hole.holeTees.find(holeTee => holeTee.teeTypeId === teeType.id);
+        if (value !== undefined) {
+          teeType.selected = true;
+        }
+      });
       // this.editChara cter = this.entityService.clone(this.character);
     } else {
       // this.gotoCharacters();
     }
   }
 
+  public onTeeTypeChange(teeType) {
 
+    // loop through holes
+    this.course.holes.forEach((hole) => {
+
+      // find the holeTee associcated with the selected id
+      const value = hole.holeTees.find(holeTee => holeTee.teeTypeId === teeType.id);
+
+      // if the tee type is not selected, remove the holeTee
+      if (!teeType.selected) {
+        if (value !== undefined) {
+          const index = hole.holeTees.indexOf(value);
+          hole.holeTees.splice(index, 1);
+        }
+      // if the tee type is selected but we dont have a holeTee, add one
+      } else if (value === undefined) {
+        hole.holeTees.push({
+          holeId: hole.id,
+          id: -1,
+          teeTypeId: teeType.id,
+          length: 300,
+          latitude: this.course.latitude,
+          longitude: this.course.longitude
+        });
+      }
+
+    });
+  }
+}
+
+class TeeType {
+  public id: number;
+  public name: string;
+  public selected: boolean;
 }
