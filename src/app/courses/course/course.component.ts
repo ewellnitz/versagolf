ef6b498120c4f874af5ecdd90421e66391331d1d
Eric Wellnitz <me@ericwellnitz.com>
2017-10-01 21:08:16 -0400

    working on maps

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CourseService } from '../course.service';
 import { Course } from '../../models/course';
-import { TeeType } from '../../models/course';
+import { TeeType, Hole, HoleLocationType } from '../../models/course';
+// import { LatLng, AgmMarker } from '@agm/core';
+import { GeoLocation } from '../../models/geo-location';
 
 @Component({
   selector: 'app-course',
@@ -12,6 +14,8 @@ import { TeeType } from '../../models/course';
 export class CourseComponent implements OnInit {
   teeTypes: Array<TeeTypeData> = [];
   course: Course = <Course>{};
+  markers: Array<Marker> = [];
+  currentHole: Hole;
   private id: any;
   constructor(private route: ActivatedRoute,
     private router: Router, private courseService: CourseService) {
@@ -53,6 +57,74 @@ export class CourseComponent implements OnInit {
       });
   }
 
+  private clearMarkers(): void{
+    this.markers = [];
+  }
+
+//   private getLatLng(location: GeoLocation) {
+//     return new LatLng(location.latitude, location.longitude);
+// }
+
+  private setMarkers(): void {
+
+    // clear markers
+    this.clearMarkers();
+
+    // expand to include hole locaitons
+    this.currentHole.holeLocations.forEach(location => {
+      // let position = this.getLatLng(location);
+      const marker: Marker = {
+        latitude: location.latitude,
+        longitude: location.longitude,
+        label: '',
+        draggable: true,
+        iconUrl: 'assets/images/marker_flag.png'
+      };
+
+      // create location marker
+      // var image: AgmIcon = {};
+      // if (location.holeLocationTypeId === HoleLocationType.GreenCenter) {
+      //   image.url = "assets/images/marker_flag.png";
+      //   image.anchor = new google.maps.Point(31, 48);
+      // } else {
+      //   image.url = "assets/images/marker_hazard.png";
+      //   image.anchor = new google.maps.Point(25, 48);
+      // }
+
+
+      this.markers.push(marker);
+
+      // google.maps.event.addListener(marker,
+      //   "dragend",
+      //   event => {
+      //     var latLng: google.maps.LatLng = event.latLng;
+      //     location.latitude = latLng.lat();
+      //     location.longitude = latLng.lng();
+      //   });
+    });
+
+    // expand to include tee
+    // var tee = this.currentHole.holeTees[0];
+
+    // //create tee marker
+    // const position = this.getLatLng(tee);
+    // const marker = new google.maps.Marker({
+    //   position: position,
+    //   map: this.map,
+    //   icon: "assets/images/marker_tee.png",
+    //   draggable: true
+    // });
+    // google.maps.event.addListener(marker,
+    //   "dragend",
+    //   event => {
+    //     var latLng: google.maps.LatLng = event.latLng;
+    //     tee.latitude = latLng.lat();
+    //     tee.longitude = latLng.lng();
+    //   });
+
+    // this.markers.push(marker);
+  }
+
   private setEditCourse(course: Course) {
     if (course) {
       this.course = course;
@@ -67,6 +139,9 @@ export class CourseComponent implements OnInit {
           teeType.selected = true;
         }
       });
+
+      this.currentHole = this.course.holes[0];
+      this.setMarkers();
       // this.editChara cter = this.entityService.clone(this.character);
     } else {
       // this.gotoCharacters();
@@ -87,7 +162,7 @@ export class CourseComponent implements OnInit {
           const index = hole.holeTees.indexOf(value);
           hole.holeTees.splice(index, 1);
         }
-      // if the tee type is selected but we dont have a holeTee, add one
+        // if the tee type is selected but we dont have a holeTee, add one
       } else if (value === undefined) {
         hole.holeTees.push({
           holeId: hole.id,
@@ -107,4 +182,13 @@ class TeeTypeData {
   public id: number;
   public name: string;
   public selected: boolean;
+}
+
+// just an interface for type safety.
+class Marker {
+  latitude: number;
+  longitude: number;
+  label?: string;
+  draggable: boolean;
+  iconUrl: string;
 }
